f96370f4ce3542700ea20a1c1111c6fa81f9953b
parimal00 <parimalbhattarai00@gmail.com>
2022-11-17 22:01:20 +0545

    final commit

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Route::get('/{any}', function () {
-//     return view('welcome');
-// })->where("any", ".*");
-
-
-Route::get('test', function () {
-  return Carbon::tomorrow()->format('y-m-d');
-});
